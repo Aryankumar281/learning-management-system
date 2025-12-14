9ed277d3617044483d6c7388986fdc40b5414054
AryanKumar281 <kumararyan5214@gmail.com>
2025-12-14 22:10:40 +0530

    done with admin dashboard analytic page

--- app/data/admin/require-admin.ts
+++ app/data/admin/require-admin.ts
@@ -2,14 +2,9 @@ import "server-only";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
-
-
-
-
-
-export async function requireAdmin() {
-    
-    const session = await auth.api.getSession({
+import {cache} from "react";
+export const requireAdmin= cache( async ()=>{
+     const session = await auth.api.getSession({
         headers: await headers(),
     });
 
@@ -22,4 +17,21 @@ export async function requireAdmin() {
     }
 
     return session;
-}
+})
+
+// export async function requireAdmin() {
+    
+//     const session = await auth.api.getSession({
+//         headers: await headers(),
+//     });
+
+//     if(!session){
+//         return redirect('/login')
+//     }
+
+//     if(session.user.role!=="admin"){
+//         return redirect('/not-admin')
+//     }
+
+//     return session;
+// }
--- app/data/user/require-user.ts
+++ app/data/user/require-user.ts
@@ -1,12 +1,13 @@
+import "server-only";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
-
-export async function requireUser() {
+import { cache } from "react";
+export const requireUser = cache(async () => {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
 
   if (!session) return redirect("/login");
   return session;
-}
+});
